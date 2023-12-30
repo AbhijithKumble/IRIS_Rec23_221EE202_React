acd55f574cbef15f405853e4290bb7ae2b157055
AbhijithKumble <abhijithkumble87564@gmail.com>
2023-12-30 12:29:01 +0530

    layout

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,7 +1,17 @@
+// import {Routes, Route, useNavigate} from 'react-router-dom';
+
+
 const Admin = () => {
     return (
         <div className="admin">
-            
+            <nav>
+                <div><button className="templates">Templates</button></div>
+                <div><button className="application">Application</button></div>
+            </nav>
+            <div className="profile">
+                <h3>Profile</h3>
+            </div>
+
         </div>
     );
 };
